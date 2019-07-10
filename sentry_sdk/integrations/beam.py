9f752116725c6c6eda3cc93a0c7eecc7c5ee9ebf
Osmar Coronel <ocoronel@sentry.io>
2019-07-10 11:12:18 -0700

    Fixed beam to match more of the integration

--- sentry_sdk/integrations/beam.py
+++ sentry_sdk/integrations/beam.py
@@ -26,14 +26,10 @@ class BeamIntegration(Integration):
         def sentry_init_pardo(self, *args, **kwargs):
             old_init(self, *args, **kwargs)
             if not getattr(self, "_sentry_is_patched", False):
-                # Need to patch both methods because older celery sometimes
-                # short-circuits to task.run if it thinks it's safe.
 
                 self.fn.process = _wrap_task_call(self.fn, self.fn.process)
 
-                # `build_tracer` is apparently called for every task
-                # invocation. Can't wrap every celery task for every invocation
-                # or we will get infinitely nested wrapper functions.
+
                 self._sentry_is_patched = True
 
             
@@ -42,26 +38,21 @@ class BeamIntegration(Integration):
 
 
 def _wrap_task_call(task, f):
-    # Need to wrap task call because the exception is caught before we get to
-    # see it. Also celery's reported stacktrace is untrustworthy.
     client =  Hub.current.client
     def _inner(*args, **kwargs):
-        exc_info = sys.exc_info()
-        _capture_exception(task, exc_info, client)
-        # try:
-        #     return f(*args, **kwargs)
-        # except Exception:
-        #     exc_info = sys.exc_info()
-        #     with capture_internal_exceptions():
-        #         _capture_exception(task, exc_info)
-        #     reraise(*exc_info)
+        try:
+            return f(*args, **kwargs)
+        except Exception:
+            exc_info = sys.exc_info()
+            with capture_internal_exceptions():
+                _capture_exception(task, exc_info, client)
+            reraise(*exc_info)
 
     return _inner
 
 def _capture_exception(task, exc_info, client):
     hub = Hub.current
-    raise Exception
-    # hub.bind_client(client)
+    hub.bind_client(client)
     if hub.get_integration(BeamIntegration) is None:
         return
     if hasattr(task, "throws") and isinstance(exc_info[1], task.throws):
@@ -76,21 +67,3 @@ def _capture_exception(task, exc_info, client):
     if hub.capture_event(event, hint=hint) is None:
         print("NOOOOOO")
 
-
-def _patch_worker_exit():
-    # Need to flush queue before worker shutdown because a crashing worker will
-    # call os._exit
-    from billiard.pool import Worker  # type: ignore
-
-    old_workloop = Worker.workloop
-
-    def sentry_workloop(*args, **kwargs):
-        try:
-            return old_workloop(*args, **kwargs)
-        finally:
-            with capture_internal_exceptions():
-                hub = Hub.current
-                if hub.get_integration(CeleryIntegration) is not None:
-                    hub.flush()
-
-    Worker.workloop = sentry_workloop
